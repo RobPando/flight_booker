64322b63e47fa358d2d7a2925f0fb28f4e1f31c3
Roberto Pando <roberto_pando@hotmail.com>
2016-12-20 10:24:34 -0300

    Fixed Seed

--- db/seeds.rb
+++ db/seeds.rb
@@ -8,13 +8,13 @@ Airport.create!(code: "SFO",
 Flight.create!(from: Airport.first,
                to: Airport.find(2),
                date: 4.months.from_now,
-               duration: 21600 )
+               duration: Time.at(21600) )
 Flight.create!(from: Airport.find(2),
                to: Airport.first,
                date: 5.months.from_now,
-               duration: 21600 )
+               duration: Time.at(21600) )
 
 Flight.create!(from: Airport.first,
                to: Airport.find(2),
                date: 9.months.from_now,
-               duration: 21600 )
+               duration: Time.at(21600) )
